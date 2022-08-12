c662e51635f05e0e0a15b85f752e9db9b3f27290
Zachary Sirna <zsirna@yahoo.com>
2022-08-12 15:05:04 -0400

    adding index and show routes for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+
+  #GET /games
+  def index
+    games = Game.all
+    render json: games
+  end
+
+  #GET /game/1
+  def show
+    game = Game.find_by(id: params[:id])
+    if game
+      render json: game
+    else
+      render json: { error:"Game not found"}, status: :not_found
+    end
+  end
+
 end
