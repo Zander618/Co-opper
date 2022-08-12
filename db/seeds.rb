32592c7bcc0399f51d03f9d773e91bccd7821178
Zachary Sirna <zsirna@yahoo.com>
2022-08-12 14:35:17 -0400

    building seeds file for games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,111 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Game.create([
+  {
+    name: "Phogs",
+    release: "12.03.2020",
+    ign_rating: nil,
+    overview: "In Phogs! you play as a two headed dog called Red and Blue. Linked by a stretchy belly, you'll need to bark, bite and bounce your way through obstacles set across the themed worlds of Food, Sleep and Play.",
+    image_url: "https://assets1.ignimgs.com/2019/08/23/phogs---button-01-1566522836204.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Brothers",
+    release: "08.07.2013",
+    ign_rating: 8.2,
+    overview: "Two brothers set out on a journey to find the 'Water of Life' and save their ailing father in Brothers: A Tale of Two Sons.",
+    image_url: "https://assets-prd.ignimgs.com/2022/01/04/brothers-a-tale-of-two-sons-button-1641279313256.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Ibb and Obb",
+    release: "08.05.2013",
+    ign_rating: 8.3,
+    overview: "Ibb and Obb is a two-player game. You have to work together to find your way through the non-newtonian world.",
+    image_url: "https://assets1.ignimgs.com/2018/12/05/ibb-and-obb---button-1543973023872.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Pode",
+    release: "05.23.2018",
+    ign_rating: nil,
+    overview: "Pode is a co-op puzzle exploration game built around the themes of friendship and cooperation, with a strong emphasis on positive actions, relaxing atmosphere and beautiful art inspired by Norwegian culture.",
+    image_url: "https://assets1.ignimgs.com/2018/03/21/pode-button-1521671866355.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Yoshi's Crafted World",
+    release: "03.29.2019",
+    ign_rating: 7.8,
+    overview: "Play as an adorable Yoshi traveling through a world crafted from household items like boxes and paper cups in the platforming adventure Yoshi’s Crafted World.",
+    image_url: "https://assets1.ignimgs.com/2019/02/21/yoshis-crafted-world---button-2-1550785233968.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Kirby and the Forgotten Land",
+    release: "03.24.2022",
+    ign_rating: 8,
+    overview: "Kirby and the Forgotten Land successfully warps the series’ classic mix of ability-based combat, platforming, and secret hunting into the third dimension.",
+    image_url: "https://assets-prd.ignimgs.com/2021/09/24/kirby-and-the-forgotten-land-button-fin-1632513789460.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Kirby: Star Allies",
+    release: "03.16.2018",
+    ign_rating: 8.3,
+    overview: "Kirby: Star Allies, the ever-friendly puffball can recruit enemies by hitting them with hearts to gather a party of up to four characters.",
+    image_url: "https://assets1.ignimgs.com/2019/11/08/kirby-star-allies---button-1573177802514.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Resident Evil Revelations 2",
+    release: "02.24.2015",
+    ign_rating: nil,
+    overview: "Uncover more secrets of the Umbrella conspiracy in Resident Evil: Revelations 2.",
+    image_url: "https://assets-prd.ignimgs.com/2021/12/22/resident-evil-revelations-2-button-1640136909836.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Biped",
+    release: "03.26.2020",
+    ign_rating: nil,
+    overview: "Two little bipedal robots, Aku and Sila, will walk side by side and embark on a fun and bonding journey.",
+    image_url: "https://assets1.ignimgs.com/2020/03/17/biped---button-00-1584409541859.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Unravel 2",
+    release: "03.22.2019",
+    ign_rating: 8.5,
+    overview: "Unravel Two enters on the bond between two Yarnys. Its gameplay can be played as a single player or local co-op drop-in/drop-out experience.",
+    image_url: "https://assets1.ignimgs.com/2018/06/09/unravel-2---button-1528578887401.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "39 Days to Mars",
+    release: "04.25.2018",
+    ign_rating: nil,
+    overview: "Embark on the 19th century's most unreliable spaceship in this 2-player couch co-op steampunk puzzle-adventure game. Play locally with a friend, or solo with the ship's cat.",
+    image_url: "https://assets1.ignimgs.com/2019/10/30/39-days-to-mars---button-fin-1572474658249.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Super Mario 3D World",
+    release: "11.22.2013",
+    ign_rating: 9.6,
+    overview: "Super Mario 3D World once again unites Mario, Luigi, Princess Peach and Toad for an all-new adventure fusing the unique platforming gameplay of Super Mario 3D Land with the cooperative gameplay of New Super Mario and the special team powers of Super Mario 2.",
+    image_url: "https://assets1.ignimgs.com/2020/01/30/super-mario-3d-world---button-2020-1580421829688.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Luigi's Mansion 3",
+    release: "10.30.2019",
+    ign_rating: 8.3,
+    overview: "Luigi's Mansion 3 is an action-adventure game, in which players control the character of Luigi from a fixed third-person perspective, as they capture ghosts across a large hotel setting.",
+    image_url: "https://assets-prd.ignimgs.com/2022/01/08/luigis-mansion-3-button-fin-1641602466276.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Untitled Goose Game",
+    release: "09.20.2019",
+    ign_rating: 8,
+    overview: "Untitled Goose Game is a slapstick-stealth-sandbox, where you are a goose let loose on an unsuspecting village.",
+    image_url: "https://assets-prd.ignimgs.com/2022/03/31/untitled-goose-game-1648735709960.jpg?width=300&crop=1%3A1%2Csmart"
+  },
+  {
+    name: "Arise: A Simple Story",
+    release: "12.03.2019",
+    ign_rating: nil,
+    overview: "Arise is an emotional journey through the life of two people where memories come alive and time bends to the player’s will.",
+    image_url: "https://assets-prd.ignimgs.com/2022/04/01/arise-a-simple-story-button-fin-1648775814805.jpg?width=300&crop=1%3A1%2Csmart"
+  }
+])
