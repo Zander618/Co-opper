7eada5641ed2fb555f236e76f79e7af4022907ed
Zachary Sirna <zsirna@yahoo.com>
2023-06-30 11:07:44 -0400

    removing console log from home page

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const loginUser = (currentUser) => {
-    console.log(currentUser)
     setUser(currentUser);
     setLoggedIn(true);
   };
