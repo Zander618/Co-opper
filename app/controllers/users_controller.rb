92e3534c11286cacccc17abcf37b3fbc30c015d3
Zachary Sirna <zsirna@yahoo.com>
2023-07-07 16:23:25 -0400

    updating users controller to fix unauthorized error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,13 @@ class UsersController < ApplicationController
   skip_before_action :authorize, only: [:create, :index]
 
   def create
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    render json: user, status: :created
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
