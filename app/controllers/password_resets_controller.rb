5ec1f0b94f86acab7209e7ae052953e0ea70afd9
Zachary Sirna <zsirna@yahoo.com>
2023-07-14 23:35:54 -0400

    updating passord resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,9 +2,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user
-      # Generate and send the password reset email here
-      # You can use a gem like `mail` or `action_mailer` to send the email
-      # Include a unique token or URL in the email for password reset verification
+      UserMailer.reset_password_email(user).deliver_now
       render json: { message: "Password reset email sent" }, status: :ok
     else
       render json: { errors: ["User not found"] }, status: :not_found
