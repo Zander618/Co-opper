4c65242cd737d6cf5f6ddad8adf52705ed323ca8
Zachary Sirna <zsirna@yahoo.com>
2023-07-17 12:08:51 -0400

    changed smtp settings to git ignored

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,15 +37,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            'ZSApplicationsMailer@gmail.com',
-  password:             'TestForProject1234',
-  authentication:       'plain',
-  enable_starttls_auto: true
-  }
+  config.action_mailer.smtp_settings = "/etc/secrets/mailer-info.json"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
