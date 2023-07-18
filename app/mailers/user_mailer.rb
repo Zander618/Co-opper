8fe2191adeecf2f4927ed9dc65341a576b4438a7
Zachary Sirna <zsirna@yahoo.com>
2023-07-18 13:55:44 -0400

    changing the password token to a password url in the user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = user
-    @reset_password_token = SecureRandom.urlsafe_base64
+    @reset_password_url = url_for(controller: 'password_resets', action: 'edit', token: @reset_password_token)
 
     mail(to: @user.email, subject: 'Reset Your Password')
   end
