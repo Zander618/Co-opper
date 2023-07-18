84d464235fbe1822d7e5b2967e47d31be94b5cbf
Zachary Sirna <zsirna@yahoo.com>
2023-07-18 14:31:53 -0400

    rebuilding User mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,12 +2,13 @@ class PasswordResetsController < ApplicationController
   skip_before_action :authorize, only: [:create]
 
   def create
-    user = User.find_by(email: params[:email])
-    if user
-      UserMailer.reset_password_email(user).deliver_now
-      render json: { message: "Password reset email sent" }, status: :ok
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      UserMailer.with(user: @user).reset.deliver_later
     else
       render json: { errors: ["User not found"] }, status: :not_found
     end
+
+    redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,6 @@
 class UserMailer < ApplicationMailer
-  def reset_password_email(user)
-    @user = user
-    @reset_password_url = url_for(controller: 'password_resets', action: 'edit', token: @reset_password_token)
-
-    mail(to: @user.email, subject: 'Reset Your Password')
+  def reset
+    @token = params[:user].signed_id(purpose: "password_reset", expires_in: 15.minutes)
+    mail to: params[:user].email
   end
 end
