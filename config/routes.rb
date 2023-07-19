52dd3495c80e72d1e4894fc52fbcba69a2904caa
Zachary Sirna <zsirna@yahoo.com>
2023-07-19 13:42:14 -0400

    adding new routes for the password reset email

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   get "/favorite_game", to:"users#favorite_game"
 
   post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "paasword_resets#update"
   
 
 end
