95b63f25cf659eb362992fa9d5b93e8680ff536b
Zachary Sirna <zsirna@yahoo.com>
2023-07-19 14:31:09 -0400

    password reset is being labeled as a 204 but I am not seeing it

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,4 @@
 class PasswordResetsController < ApplicationController
-  include ActionController::Cookies
   skip_before_action :authorize, only: [:create]
 
   def create
@@ -9,7 +8,5 @@ class PasswordResetsController < ApplicationController
     else
       render json: { errors: ["User not found"] }, status: :not_found
     end
-
-    redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
   end
 end
