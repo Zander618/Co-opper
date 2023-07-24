9d7bd2b13c8df3d83e3cf2d9501d10a928cbb2a4
Zachary Sirna <zsirna@yahoo.com>
2023-07-24 14:06:29 -0400

    adding success message in PasswordResetController

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,6 +5,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user.present?
       UserMailer.reset(@user).deliver_later
+      render json: { message: "Password reset email sent successfully." }, status: :ok
     else
       render json: { errors: ["User not found"] }, status: :not_found
     end
