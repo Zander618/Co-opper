6fac1e1abe454c2a4b1e4422bf0ba1c987d2a628
Zachary Sirna <zsirna@yahoo.com>
2023-07-26 15:37:39 -0400

    adding validations to user for password and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,23 @@ class User < ApplicationRecord
   
   validates :username, uniqueness: true
   validates :username, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: {minimum: 6}, if: :password
+
+  def send_password_reset
+    self.update!(password_reset_token: self.generate_base64_token, password_reset_sent_at: Time.zone.now)
+    UserMailer.with(user: self, token: self.password_reset_token).password_reset_email.deliver_now
+  end
+
+  def reset_password(params)
+      self.update!(password: params[:password], password_confirmation: params[:password_confirmation])      
+      self.update!(password_reset_token: nil, password_reset_sent_at: nil)    
+  end
+
+  private
+
+  def generate_base64_token
+    SecureRandom.urlsafe_base64
+  end
+
 end
