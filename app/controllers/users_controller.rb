571ec3fda578c0b891267169a3b18e7c9aeb2371
Zachary Sirna <zsirna@yahoo.com>
2023-07-26 15:54:04 -0400

    adding reset method to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def reset
+    user = User.find_by(email: params[:email])
+    if user
+      if user.password_reset_sent_at && user.password_reset_sent_at + 3600 >= Time.zone.now && user.password_reset_token == params[:token]
+        user.reset_password(params)
+        session[:user_id] = user.id
+        render json: {user: user, alerts: ["Your password has been reset."] }, status: :created
+      else
+        render json: { errors: ["Token has expired."] }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: ["Email address not registered."] }, status: :not_found
+    end
+  end
+
   private
 
   def user_params
