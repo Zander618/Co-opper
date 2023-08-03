72a62c79933d4a890cf98ba04622dee917593b43
Zachary Sirna <zsirna@yahoo.com>
2023-08-03 14:59:09 -0400

    adding new column to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_26_203126) do
+ActiveRecord::Schema.define(version: 2023_08_03_185817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2023_07_26_203126) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
